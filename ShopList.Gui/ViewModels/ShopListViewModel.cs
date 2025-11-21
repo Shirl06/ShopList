1b5e082e7c020bbd0d9efde5020ef65700f652bb
Shirley Coyoc <24470067@campeche.tecnm.mx>
2025-11-21 11:12:52 -0600

    Utiliza MVVM con CommunityToolkit.Mvvm

--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -1,53 +1,25 @@
-﻿using ShopList.Gui.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShopList.Gui.Models;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
 {
-    public class ShopListViewModel : INotifyPropertyChanged
+    public partial class ShopListViewModel : ObservableObject
     {
+        [ObservableProperty]
         private string _nombreDelArticulo = string.Empty;
+        [ObservableProperty]
         private int _cantidadAComprar = 1;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
         public ObservableCollection<Item> Items { get; }
-        public string NombreDelArticulo
-        {
-            get => _nombreDelArticulo;
-            set
-            {
-                if (value != _nombreDelArticulo)
-                {
-                    _nombreDelArticulo = value;
-                    OnPropertyChanged(nameof(NombreDelArticulo));
-                }
-            }
-        }
-        public int CantidadAComprar
-        {
-            get => _cantidadAComprar;
-            set
-            {
-                if (value != _cantidadAComprar)
-                {
-                    _cantidadAComprar = value;
-                    OnPropertyChanged(nameof(CantidadAComprar));
-                }
-            }
-        }
-        public ICommand AgregarShopListItemCommand {
-            get;
-            private set; 
-        }
+       
         public ShopListViewModel()
         {
             Items = new ObservableCollection<Item>();
             CargarDatos();
-            AgregarShopListItemCommand = 
-                new Command(AgregarShopListItem);
+           
         }
+        [RelayCommand]
         public void AgregarShopListItem()
         {
             if (string.IsNullOrEmpty(NombreDelArticulo) 
@@ -67,11 +39,11 @@ namespace ShopList.Gui.ViewModels
             NombreDelArticulo = string.Empty;
             CantidadAComprar = 1;
         }
+        [RelayCommand]
         public void EliminarShopListItem()
         {
 
         }
-
         private void CargarDatos()
         {
             Items.Add(new Item()
@@ -95,13 +67,6 @@ namespace ShopList.Gui.ViewModels
                 Cantidad = 500,
                 Comprado = false,
             });
-        }
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(
-                this,
-                new PropertyChangedEventArgs(propertyName)
-                );
         }
     }
 }
