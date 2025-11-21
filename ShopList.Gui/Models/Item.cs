28c327d1854c12c9fba78f273353a865aea433ea
Shirley Coyoc <24470067@campeche.tecnm.mx>
2025-11-21 10:39:37 -0600

    Utiliza el patróń MVVM sin bibliotecas

--- ShopList.Gui/Models/Item.cs
+++ ShopList.Gui/Models/Item.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace ShopList.Gui.Models
 {
@@ -11,6 +7,7 @@ namespace ShopList.Gui.Models
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public int Cantidad { get; set; }
+        public bool Comprado { get; set; } = false;
 
         public override string ToString()
         {
--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -1,16 +1,75 @@
 ﻿using ShopList.Gui.Models;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
 {
-    public class ShopListViewModel
+    public class ShopListViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Item> Items { get; }
+        private string _nombreDelArticulo = string.Empty;
+        private int _cantidadAComprar = 1;
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Item> Items { get; }
+        public string NombreDelArticulo
+        {
+            get => _nombreDelArticulo;
+            set
+            {
+                if (value != _nombreDelArticulo)
+                {
+                    _nombreDelArticulo = value;
+                    OnPropertyChanged(nameof(NombreDelArticulo));
+                }
+            }
+        }
+        public int CantidadAComprar
+        {
+            get => _cantidadAComprar;
+            set
+            {
+                if (value != _cantidadAComprar)
+                {
+                    _cantidadAComprar = value;
+                    OnPropertyChanged(nameof(CantidadAComprar));
+                }
+            }
+        }
+        public ICommand AgregarShopListItemCommand {
+            get;
+            private set; 
+        }
         public ShopListViewModel()
         {
             Items = new ObservableCollection<Item>();
             CargarDatos();
+            AgregarShopListItemCommand = 
+                new Command(AgregarShopListItem);
+        }
+        public void AgregarShopListItem()
+        {
+            if (string.IsNullOrEmpty(NombreDelArticulo) 
+                || CantidadAComprar <= 0)
+            {
+                return;
+            }
+            Random generador = new Random();
+            var item = new Item
+            {
+                Id =generador.Next(),
+                Nombre = NombreDelArticulo,
+                Cantidad = CantidadAComprar,
+                Comprado = false
+            };
+            Items.Add(item);
+            NombreDelArticulo = string.Empty;
+            CantidadAComprar = 1;
+        }
+        public void EliminarShopListItem()
+        {
+
         }
 
         private void CargarDatos()
@@ -20,19 +79,29 @@ namespace ShopList.Gui.ViewModels
                 Id = 1,
                 Nombre = "Leche",
                 Cantidad = 2,
+                Comprado = false,
             }); 
             Items.Add(new Item()
             {
                 Id = 2,
                 Nombre = "Pan de caja",
                 Cantidad = 1,
+                Comprado = false,
             }); 
             Items.Add(new Item()
             {
                 Id = 3,
                 Nombre = "Jamón",
                 Cantidad = 500,
+                Comprado = false,
             });
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(propertyName)
+                );
         }
     }
 }
